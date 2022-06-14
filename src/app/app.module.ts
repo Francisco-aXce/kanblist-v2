1c64c0ef1c3d1d8e71829053cdb8f5f0cf31bb30
Francisco Ramos <jorge.ramos.francisco.15@gmail.com>
2022-06-14 19:21:57 -0300

    Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { BoardsComponent } from './website/pages/boards/boards.component';
+import { ListsComponent } from './website/pages/lists/lists.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BoardsComponent,
+    ListsComponent
   ],
   imports: [
     BrowserModule,
